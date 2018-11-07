3f320cc9f0f83b6e663f93e5fb7d6b550ce33e50
ElijahButers <Elijahbuters@gmail.com>
2018-11-07 16:41:12 +0200

    Initial Commit

---
+++ Quote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quote
+//
+//  Created by User on 11/7/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
