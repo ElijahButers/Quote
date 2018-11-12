3c4f9a8d3e7e6eae1f4341be958ab723bd521616
ElijahButers <Elijahbuters@gmail.com>
2018-11-12 17:16:30 +0200

    Create a new swift file named EditViewController class, set it as the new view controller’s suctom class.

---
+++ Quote/EditViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EditViewController.swift
+//  Quote
+//
+//  Created by User on 11/12/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+    
+}
