2ba26b4618b438e951a46dbd575d8aa5cdbbae9f
ElijahButers <Elijahbuters@gmail.com>
2018-11-12 10:52:09 +0200

    Connect two labels as outlests to the ViewController class that will represent the quote text and quote author.

--- Quote/ViewController.swift
+++ Quote/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteTextLabel: UILabel!
+    @IBOutlet weak var quoteAuthorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
